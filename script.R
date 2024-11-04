434e6d280af45064d5a6f7380314cd1ad06b0e67
Tarek Omran <to23abo@herts.ac.uk>
2024-11-04 09:18:22 +0000

    Filtered the data and visualized it on a histogram

--- script.R
+++ script.R
@@ -4,9 +4,15 @@ library(readr)
 #Allocating variable "df" as a reference to the dataset DS301 
 df<-read.csv("HRDataset_v14.csv")
 
+#Created a subset of the original dataset to clean the data
+df2<-subset(df,Salary<100000)
+
+#Visualize the frequency of Salaries based on the cleaned data which looks normal
+hist(df2$Salary, main = "Histogram of Salary Distribution", xlab = "Salaries")
+
 #Visualized the data using a boxplot function with Salary as x-axis and Performance Score as y-axis
-boxplot(df$Salary ~ df$PerformanceScore, xlab =
+boxplot(df2$Salary ~ df2$PerformanceScore, xlab =
           "Perforamnce Score", ylab = "Salary", main = "Salary on Performance Score")
 
 #Calculating the mean of all salaries
-mean(df$Salary)
+mean(df2$Salary)
