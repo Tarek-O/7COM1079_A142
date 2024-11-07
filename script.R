5accc5ea9f70b1899092da847372ebf7cefd32d2
Aaju Maharjan <am24agk@herts.ac.uk>
2024-11-07 18:35:00 +0000

    enhancement(Histogram) add curve, chore(Constants) move constants to top of script, style(Box plot) add colors and legend - add a curve to the histogram - move constant declaration to the top of the script for not cluttering relevant blocks - add colors to the box plot based on the severity of the performance score - add legend to the box plot to show the mean value
    ISSUE RESOLVED: Only using data from Massachusetts

--- script.R
+++ script.R
@@ -8,23 +8,45 @@ library(dplyr)
 #Allocating variable "df" as a reference to the dataset DS301 
 raw_data<-read.csv("HRDataset_v14.csv")
 
-#Created a subset of the original dataset to clean the data
-### Is there a purpose to this? Just for clarity are we only doing our research in the state of massachusetts? ###
-massachusetts_employee_data<-subset(raw_data, State == "MA")
-
-# Extract the variables used for analysis (Performance Score, Salary) along with the Details of the Employee
+# Constants
 EMPLOYEE_COLUMNS <- c("EmpID", "Employee_Name")
 FEATURE_COLUMNS <- c("PerformanceScore", "Salary")
 FINAL_COLUMNS <- c(EMPLOYEE_COLUMNS, FEATURE_COLUMNS)
+DEPENDENT_VARIABLE <- "PerformanceScore"
+INDEPENDENT_VARIABLE <- "Salary"
+
+#Created a subset of the original dataset to clean the data
+massachusetts_employee_data<-subset(raw_data, State == "MA")
+
+# Extract the variables used for analysis (Performance Score, Salary) along with the Details of the Employee
 massachusetts_employee_data_projection <- massachusetts_employee_data[FINAL_COLUMNS]
 
 # Visualize the frequency of Salaries based on the cleaned data which looks normal
-hist(massachusetts_employee_data_projection$Salary, main = "Histogram of Salary Distribution", xlab = "Salaries", xaxt = "n")
-axis(1, at = pretty(massachusetts_employee_data_projection$Salary), labels = format(pretty(massachusetts_employee_data_projection$Salary), scientific = FALSE))
+hist(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]],
+     main = "Histogram of Salary Distribution",
+     xlab = "Salaries",
+     xaxt = "n",
+     prob = TRUE)
+# Format the x-axis
+axis(1, at = pretty(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]]),
+     labels = format(pretty(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]]), scientific = FALSE))
+# Add a bell curve
+curve(dnorm(x, mean = mean(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]]),
+            sd = sd(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]])),
+      add = TRUE, col = "blue", lwd = 2)
+
+# wilcox.test(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]] ~ massachusetts_employee_data_projection[[DEPENDENT_VARIABLE]])
 
 #Visualized the data using a boxplot function with Salary as x-axis and Performance Score as y-axis
-boxplot(massachusetts_employee_data_projection$Salary ~ massachusetts_employee_data_projection$PerformanceScore, xlab =
-          "Perforamnce Score", ylab = "Salary", main = "Salary Based on Performance Score", las = 1)
-
+boxplot(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]] ~ massachusetts_employee_data_projection[[DEPENDENT_VARIABLE]],
+        xlab = "Perforamnce Score",
+        ylab = "Salary",
+        main = "Salary Based on Performance Score",
+        las = 1,
+        col = c("lightgreen", "lightblue", "orange", "red"))
+abline(h = mean(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]]), col = "red", lwd = 2)
 #Calculating the mean of all salaries
-mean(massachusetts_employee_data_projection$Salary)
+mean_salary <- mean(massachusetts_employee_data_projection[[INDEPENDENT_VARIABLE]])
+
+# Add a legend to the boxplot to show the mean salary
+legend("topright", legend = paste("Mean Salary: ", round(mean_salary, 2)), fill = "red")
