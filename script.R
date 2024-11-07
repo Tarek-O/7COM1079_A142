996189b38f79731b22c5bdf8173221e221954dc1
Tarek Omran <to23abo@herts.ac.uk>
2024-11-07 16:24:18 +0000

    Changed Subset from Salary to State. Added comment where an error exists

--- script.R
+++ script.R
@@ -9,13 +9,14 @@ library(dplyr)
 df<-read.csv("HRDataset_v14.csv")
 
 #Created a subset of the original dataset to clean the data
-df2<-subset(df,Salary<100000)
+df2<-subset(df,State == "MA")
 
 # Extract the variables used for analysis (Performance Score, Salary) along with the Details of the Employee
 EMPLOYEE_COLUMNS <- c("EmpID", "Employee_Name", "PerformanceScore", "Salary")
 df3 <- df2[EMPLOYEE_COLUMNS]
 
 #Visualize the frequency of Salaries based on the cleaned data which looks normal
+### THERE IS AN ERROR IN HISTOGRAM FUNCTION###
 hist(df2$Salary, format(df2$Salary, scientific = F), main = "Histogram of Salary Distribution", xlab = "Salaries")
 
 #Visualized the data using a boxplot function with Salary as x-axis and Performance Score as y-axis
